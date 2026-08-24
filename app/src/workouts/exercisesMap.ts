bf3709b76fa5dfbd3c5b403b5aa036da1c0a3f63
Marcin <mdszczepanski@gmail.com>
2026-08-24 08:13:24 +0200

    fix: remove redundant broken exercise map

--- app/src/workouts/exercisesMap.ts
+++
@@ -1,5 +0,0 @@
-import { EXERCISES, type ExerciseIdentity } from './exercises';
-
-export const EXERCISES_MAP: Map<string, ExerciseIdentity> = new Map(
-  EXERCISES.map(exercise => [exercise.id, exercise])
-);
